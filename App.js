6895798aa682a9c43248e7f064c1bfa80d3bcfd9
Ayda-22 <120122647+Ayda-22@users.noreply.github.com>
2024-05-27 20:38:55 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-// App.js
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import EventPage from "./components/EventPage";
-import About from "./components/About";
 import Navbar from "./components/Navbar";
+
+// Lazy load components
+const Home = lazy(() => import("./components/Home"));
+const EventPage = lazy(() => import("./components/EventPage"));
+const About = lazy(() => import("./components/About"));
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/event/:id" component={EventPage} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/event/:id" component={EventPage} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
